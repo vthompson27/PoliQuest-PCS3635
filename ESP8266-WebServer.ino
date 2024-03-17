7c7363c5297a78f0e2490cc39c3b8327fe8a1ed1
BrunoSchioF <157405538+BrunoSchioF@users.noreply.github.com>
2024-03-17 11:02:44 -0300

    Add files via upload

---
+++ ESP8266-WebServer.ino
@@ -0,0 +1,41 @@
+//============================================
+//ESP8266 Web Server (HTML + CSS + JavaScript)
+//============================================
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include "webpageCode.h"
+#include "gamemode1Code.h"
+//------------------------------------------
+ESP8266WebServer server(80);
+const char* ssid = "OGATA";
+const char* password = "Gilberto101";
+//------------------------------------------
+void webpage()
+{
+  server.send(200,"text/html", webpageCode);
+}
+
+void gamemode1()
+{
+  server.send(200,"text/html", gamemode1Code);
+}
+
+//=================================================================
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while(WiFi.status()!=WL_CONNECTED){delay(500);Serial.print(".");}
+  Serial.println();
+  Serial.print("IP Address: "); Serial.println(WiFi.localIP());
+
+  server.on("/", webpage);
+  server.on("/mode1", gamemode1);
+
+  server.begin();
+}
+//=================================================================
+void loop()
+{
+  server.handleClient();
+}
